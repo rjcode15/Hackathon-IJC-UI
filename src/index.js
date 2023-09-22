7b842da1d05e4229892237960d6935271b09f68c
Anna <annamedyukh@gmail.com>
2023-09-22 15:15:57 -0400

    fixed issue

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Reducer from './reducer';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
 const store = createStore(Reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
